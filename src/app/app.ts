1e7abbf1cebb6bf6846f3afc4bbeeae637a0c104
Cedust <t.sudeck@live.de>
2026-01-15 18:28:59 +0100

    added menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,21 @@ import { Component, signal } from '@angular/core';
 import { CharacterArea } from './character-area/character-area';
 import { GameArea } from './game-area/game-area';
 import { InventoryArea } from './inventory-area/inventory-area';
+import { Menu } from './menu/menu';
+import { MenuService } from '../shared/services';
 
 @Component({
   selector: 'app-root',
-  imports: [GameArea, CharacterArea, InventoryArea],
+  imports: [GameArea, CharacterArea, InventoryArea, Menu],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('Idle Hero');
+
+  protected get IsMenuOpen(): boolean {
+    return this.menuService.IsMenuOpen();
+  }
+
+  constructor(private menuService: MenuService) {}
 }
---
+++ src/app/menu/menu.ts
@@ -0,0 +1,45 @@
+import { Component, ElementRef, HostListener, inject } from '@angular/core';
+
+import { MenuService } from '../../shared/services';
+import { Separator } from '../../shared/components';
+import { environment } from '../../environment/environment';
+
+@Component({
+  selector: 'app-menu',
+  imports: [Separator],
+  templateUrl: './menu.html',
+  styleUrl: './menu.scss'
+})
+export class Menu {
+  private elementRef = inject(ElementRef);
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.CloseMenu();
+    }
+  }
+
+  protected get Version(): string {
+    return environment.version;
+  }
+
+  protected get IsMenuOpen(): boolean {
+    return this.menuService.IsMenuOpen();
+  }
+
+  constructor(private menuService: MenuService) {}
+
+  private CloseMenu() {
+    this.menuService.IsMenuOpen.set(false);
+  }
+
+  ToggleMenu() {
+    this.menuService.IsMenuOpen.set(!this.menuService.IsMenuOpen());
+  }
+
+  Reload() {
+    window.location.reload();
+    this.CloseMenu();
+  }
+}
