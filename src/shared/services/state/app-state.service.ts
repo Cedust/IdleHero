5ca0dbc9b0e9d5a1cc9fd4cea94774e3e963d3e0
Cedust <t.sudeck@live.de>
2026-02-27 20:05:09 +0100

    changed default area to dungeon area

--- src/shared/services/state/app-state.service.ts
+++ src/shared/services/state/app-state.service.ts
@@ -1,6 +1,16 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AppStateService {
+  private router = inject(Router);
+
   public readonly LoadedExistingSaveGame = signal(false);
+  public readonly CurrentArea = signal<'Town' | 'Dungeon'>('Dungeon');
+
+  public SwitchAreaTo(area: 'Town' | 'Dungeon'): void {
+    this.CurrentArea.set(area);
+    this.router.navigate([`/game/${area.toLowerCase()}`]);
+  }
 }
